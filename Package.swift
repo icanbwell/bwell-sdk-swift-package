d89b89984eecc6e7725c1b6d9866d5d826c9fa64
Bill <71160194+wfieldx@users.noreply.github.com>
2026-04-06 11:10:37 -0500

    EA-2147 Fix corrupted Package.swift, rename target to BWellSDK (#5)

--- Package.swift
+++ Package.swift
@@ -10,26 +10,12 @@ let package = Package(
     products: [
         .library(
             name: "BWellSDK",
-            targets: ["BWell"]
-        )
-    ],
-    dependencies: [
-        .package(
-            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.0.0-20260406.143728-658/BWellSDK-1.0.0-20260406.143728-658.xcframework.zip",
-            .upToNextMajor(from: "1.25.3")
-        ),
-        .package(
-            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.0.0-20260406.143728-658/BWellSDK-1.0.0-20260406.143728-658.xcframework.zip",
-            from: "2.2.0"
-        ),
-        .package(
-            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.0.0-20260406.143728-658/BWellSDK-1.0.0-20260406.143728-658.xcframework.zip",
-            from: "2.2.0"
+            targets: ["BWellSDK"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "BWell",
+            name: "BWellSDK",
             url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.0.0-20260406.143728-658/BWellSDK-1.0.0-20260406.143728-658.xcframework.zip",
             checksum: "50045a6b416da2d98f32206257ce0306a743f6f98589abef0e289ed077ed22c7"
         )
