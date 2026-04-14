3c6e495fe80a168e7f5940658a464f604da72036
wfieldx <71160194+wfieldx@users.noreply.github.com>
2026-04-14 14:02:42 -0500

    CIE-7862 Update Artifactory URL to artifacts.bwell.com

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BWellSDK",
-            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-release/com/bwell/bwell-sdk-swift/1.0.0-beta/BWellSDK-1.0.0-beta.xcframework.zip",
+            url: "https://artifacts.bwell.com/artifactory/bwell-sdk-swift-release/com/bwell/bwell-sdk-swift/1.0.0-beta/BWellSDK-1.0.0-beta.xcframework.zip",
             checksum: "35a6be918dedb9907bbcd533082ad637f59d4e87c13e8d871aa3498687ca6ed4"
         )
     ]
