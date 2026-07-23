cd9e78bef759e768377d4433234b638ec7675699
bwell-dev <bwell-dev@icanbwell.com>
2026-07-23 15:51:18 +0000

    Snapshot 1.5.0-20260723.153713-703

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BWellSDK",
-            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.5.0-20260717.154331-700/BWellSDK-1.5.0-20260717.154331-700.xcframework.zip",
-            checksum: "2b11f75add910bebb7891ad7559fdf60e72d71d3e5cb3d5bb8026908f07a506d"
+            url: "https://icanbwell.jfrog.io/artifactory/bwell-sdk-swift-snapshot/com/bwell/bwell-sdk-swift/1.5.0-20260723.153713-703/BWellSDK-1.5.0-20260723.153713-703.xcframework.zip",
+            checksum: "1a7caa1419194ad48c2671e2749fcd0b416797a79bc4a310086e8520e8b10b7b"
         )
     ]
 )
